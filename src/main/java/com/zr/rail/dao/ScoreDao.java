c2122e19d76ede6df4a9ea09fc4d0c560e357ec0
Tomcatkai <tomcatkai@gmail.com>
2018-09-12 17:16:56 +0800

    删除多余注释及废弃代码

--- src/main/java/com/zr/rail/dao/ScoreDao.java
+++ src/main/java/com/zr/rail/dao/ScoreDao.java
@@ -7,22 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ScoreDao {
 
-//    /**
-//     * 通过学生id查询该学生的所有成绩列表
-//     * @param stuId id
-//     * @return 学生
-//     */
-//    @Select("SELECT * FROM tbl_score WHERE score_user_id = #{scoreUserId}")
-//    @Results({
-//            @Result(property = "scoreId",   column = "score_id"),
-//            @Result(property = "scoreUserId", column = "score_user_id"),
-//            @Result(property = "scoreDistance", column = "score_distance"),
-//            @Result(property = "scoreSpeed",   column = "score_speed"),
-//            @Result(property = "scoreTotal",  column = "score_total")
-//    })
-//    Score getOneFromId(Long stuId);
-
-
     /**
      * 插入成绩信息
      * @param score 成绩对象
@@ -31,11 +15,4 @@ public interface ScoreDao {
     @Options(useGeneratedKeys = true, keyProperty = "scoreId", keyColumn="score_id")
     void insert(Score score);
 
-
-//    /**
-//     * 删除学生信息
-//     * @param stuId 学生id
-//     */
-//    @Delete("DELETE FROM tbl_student WHERE stu_id =#{stuId}")
-//    void delete(Long stuId);
 }
